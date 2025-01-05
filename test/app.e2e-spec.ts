1818c7bff6cc0dca2e10bd30f38967f7c944b347
Hesameddin Mosaffa <hesam.mosaffa.1251@gmail.com>
2025-01-05 23:53:50 +0330

    updated e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
 import { AppModule } from './../src/app.module';
 import { INestApplication } from '@nestjs/common';
+import { CreateUserDto } from './../src/dto/create-user.dto';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -19,10 +20,75 @@ describe('AppController (e2e)', () => {
     await app.close();
   });
 
-  it('/ (GET)', () => {
+
+  it('/create-user (POST)', () => {
+    const createUserDto: CreateUserDto = { name: 'hesam', email: 'hesam.mosaffa.1251@gmail.com' };
     return request(app.getHttpServer())
-      .get('/')
+      .post('/create-user')
+      .send(createUserDto)
+      .expect(201)
+      .expect((res) => {
+        expect(res.body.name).toBe(createUserDto.name);
+        expect(res.body.email).toBe(createUserDto.email);
+      });
+  });
+
+  it('/get-users/:id (GET)', async () => {
+    const createUserDto: CreateUserDto = { name: 'hesam', email: 'hesam.mosaffa.1251@gmail.com' };
+    const user = await request(app.getHttpServer())
+      .post('/create-user')
+      .send(createUserDto)
+      .expect(201);
+
+    return request(app.getHttpServer())
+      .get(`/get-users/${user.body._id}`)
       .expect(200)
-      .expect('Hello World!');
+      .expect((res) => {
+        expect(res.body.name).toBe(createUserDto.name);
+        expect(res.body.email).toBe(createUserDto.email);
+      });
+  });
+
+  it('/delete-user/:id (DELETE)', async () => {
+    const createUserDto: CreateUserDto = { name: 'hesam', email: 'hesam.mosaffa.1251@gmail.com' };
+    const user = await request(app.getHttpServer())
+      .post('/create-user')
+      .send(createUserDto)
+      .expect(201);
+
+    return request(app.getHttpServer())
+      .delete(`/delete-user/${user.body._id}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.message).toBe('User and associated companies deleted successfully');
+      });
+  });
+
+  it('/update-user/:id (PUT)', async () => {
+    const createUserDto: CreateUserDto = { name: 'hesam', email: 'hesam.mosaffa.1251@gmail.com' };
+    const user = await request(app.getHttpServer())
+      .post('/create-user')
+      .send(createUserDto)
+      .expect(201);
+
+    const updateUserDto: Partial<CreateUserDto> = { name: 'Hesameddin Mosaffa' };
+
+    return request(app.getHttpServer())
+      .put(`/update-user/${user.body._id}`)
+      .send(updateUserDto)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.name).toBe(updateUserDto.name);
+        expect(res.body.email).toBe(createUserDto.email);
+      });
+  });
+
+  it('/get-all-users (GET)', async () => {
+    return request(app.getHttpServer())
+      .get('/get-all-users')
+      .expect(200)
+      .expect((res) => {
+        expect(Array.isArray(res.body)).toBe(true);
+      });
   });
 });
